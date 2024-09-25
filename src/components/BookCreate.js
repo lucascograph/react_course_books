ad818c9e17bf1d59ce09b7d9a2a4c1e36da22a9b
unknown <lucas_rohlin@co-graph.com>
2024-09-25 09:03:44 +0900

    started project

---
+++ src/components/BookCreate.js
@@ -0,0 +1,30 @@
+import { useState } from "react";
+
+
+function BookCreate({onSubmit}) {
+    const [title, setTitle] = useState("");
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        onSubmit(title);
+        setTitle("");
+    };
+
+    const handleChange = (event) => {
+         setTitle(event.target.value);
+
+    };
+
+    return (
+        <div className="book-create">
+            <h3>Add a book</h3>
+            <form onSubmit={ handleSubmit }>
+                <label>Title:</label>
+                <input className="input" value={title} onChange={handleChange} />
+                <button className="button">Create</button>
+            </form>
+        </div>
+    );
+}
+
+export default BookCreate;
---
+++ src/components/BookList.js
@@ -0,0 +1,24 @@
+import { useContext } from "react";
+import BooksContext from "../context/books";
+import BookShow from "./BookShow";
+
+
+function BookList({books, onDelete, onEdit}) {
+    const { count, incrementCount} = useContext(BooksContext);
+
+    const renderedBooks = books.map((book) => {
+        return (
+            <BookShow key={book.id} book={book} onEdit={onEdit} onDelete={onDelete} />
+        );
+    })
+
+    return (
+        <div className="book-list">
+            {count}
+            <button onClick={incrementCount}>Click Me!</button>
+            {renderedBooks}
+        </div>
+    )
+}
+
+export default BookList; 
